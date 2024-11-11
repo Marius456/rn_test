bfe720a37dad6eb0d8ba8f8a2604632978c36866
Marius456 <marius.zilguzis@gmail.com>
2024-11-11 19:38:19 +0200

    added files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
-const { withRNVBabel } = require('@rnv/adapter');
+// const { withRNVBabel } = require('@rnv/adapter');
 
-module.exports = withRNVBabel({});
+// module.exports = withRNVBabel({});
+
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
+};
