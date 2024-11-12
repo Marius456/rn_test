5d7682c06ff68163445eb530e860aa3177e2830b
Marius456 <marius.zilguzis@gmail.com>
2024-11-12 15:02:56 +0200

    custom 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { themeStyles } from '../config';
+import { StyleSheet } from 'react-native';
+import { Stack, Link } from 'expo-router';
+import { ThemedText } from '../components/ThemedText';
+import { ThemedView } from '../components/ThemedView';
 
 export default function Page() {
     return (
-        <View style={themeStyles.container}>
-            <Text style={themeStyles.textH2}>This is custom 404!</Text>
-        </View>
+      <>
+        <Stack.Screen options={{ title: 'Oops!' }} />
+        <ThemedView style={styles.container}>
+          <ThemedText type="title">This screen doesn't exist.</ThemedText>
+          <Link href="/" style={styles.link}>
+            <ThemedText type="link">Go to home screen!</ThemedText>
+          </Link>
+        </ThemedView>
+      </>
     );
-}
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 20,
+    },
+    link: {
+      marginTop: 15,
+      paddingVertical: 15,
+    },
+  });
+  
